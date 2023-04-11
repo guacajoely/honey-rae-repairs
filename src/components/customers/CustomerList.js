e15b8c99c75af5d86cdc048135b9fe0f0ca332b0
guacajoely <guacajoely@gmail.com>
2023-04-11 14:11:20 -0400

    ch 12 complete

---
+++ src/components/customers/CustomerList.js
@@ -0,0 +1,25 @@
+import { useEffect, useState } from "react"
+import { Customer } from "./Customer.js"
+import "./Customers.css"
+
+export const CustomerList = () => {
+    const [customers, setCustomers] = useState([])
+
+    useEffect(() => {
+        fetch('http://localhost:8088/users?isStaff=false')
+            .then(response => response.json())
+            .then((customerArray) => {
+                setCustomers(customerArray)
+            })
+        }, []
+    )
+
+    return <article className="customers">
+        {
+            customers.map(customer => <Customer key={`Customer--${customer.id}`}
+                                        id={customer.id} 
+                                        fullName={customer.fullName} 
+                                        email={customer.email} />)
+        }
+    </article>
+}
